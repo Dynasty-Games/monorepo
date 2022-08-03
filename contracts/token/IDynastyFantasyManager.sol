c107668889df8c7c7ffa32b62134606352e1ecbc
vandeurenglenn <vandeurenglenn@gmail.com>
2022-08-03 18:15:19 +0200

    Create IDynastyFantasyManager.sol

---
+++ contracts/token/IDynastyFantasyManager.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import 'node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol';
+
+interface IDynastyFantasyManager is IERC20 {
+  function mint(address to_, uint256 amount_) external;
+
+  function burn(uint256 amount_) external;
+
+  function burnFrom(address from_, uint256 amount_) external;
+}
