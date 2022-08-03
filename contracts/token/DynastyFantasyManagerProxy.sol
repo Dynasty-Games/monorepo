9c2008aac464b712c8bb56d4f7ea8ea8a28c6e87
vandeurenglenn <vandeurenglenn@gmail.com>
2022-08-03 18:15:30 +0200

    Create DynastyFantasyManagerProxy.sol

---
+++ contracts/token/DynastyFantasyManagerProxy.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+import "node_modules/@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+
+contract DynastyFantasyProxyManager is TransparentUpgradeableProxy {
+  constructor(address logic, address admin, bytes memory data) TransparentUpgradeableProxy(logic, admin, data) public {}
+}
