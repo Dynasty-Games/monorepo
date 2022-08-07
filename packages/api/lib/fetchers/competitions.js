d7d43f5ceed5362dee45e9367c71f0b93f02651a
vandeurenglenn <vandeurenglenn@gmail.com>
2022-08-07 21:08:02 +0200

    api: fix typo startsWidth -> startsWith

--- packages/api/lib/fetchers/competitions.js
+++ packages/api/lib/fetchers/competitions.js
@@ -26,7 +26,7 @@ const job = async ({category, style, id}, data) => {
     price: utils.formatUnits(params.price, 8),
     portfolioSize: params.portfolioSize.toNumber(),
     participants: participants.toNumber(),
-    extraData: params.extraData.toString().startsWidth('{') ? JSON.parse(params.extraData.toString()) : {},
+    extraData: params.extraData.toString().startsWith('{') ? JSON.parse(params.extraData.toString()) : {},
     name: params.name,
     startTime: Number(params.startTime.toNumber() * 1000).toString(),
     prizePool: utils.formatUnits(params.prizePool, 8),
