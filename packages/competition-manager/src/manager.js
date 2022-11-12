c35e159379f56d8806d9c6c0c302eccdb393d99b
vandeurenglenn <vandeurenglenn@gmail.com>
2022-11-12 12:19:52 +0100

    manager: add storage

--- packages/competition-manager/src/manager.js
+++ packages/competition-manager/src/manager.js
@@ -8,11 +8,14 @@ import CompetitionABI from './../node_modules/@dynasty-games/abis/Competition-AB
 import {create, close, rankings} from './tasks/tasks.js'
 import {config} from './contracts';
 
+import DynastyStorageClient from '../../storage/src/storage-client.js';
+
 (async () => {
+  globalThis.storage = await new DynastyStorageClient()
   // await login()
   const tasks = [create(), close(), rankings()]
-  //await tasks[1].job() // try closing on each start
-// tasks[0].job()
+  // await tasks[2].job() // try closing on each start
+// await tasks[0].job()
   for (const task of tasks) {
     task.runner.start()
   }
