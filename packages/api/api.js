87336b11d30be85b54735b2ad217780f8c691cee
vandeurenglenn <vandeurenglenn@gmail.com>
2022-11-13 15:53:29 +0100

    signer-types: fix default not exported

--- packages/api/api.js
+++ packages/api/api.js
@@ -2415,6 +2415,34 @@ router$1.get('/portfolio-points', async ctx => {
   ctx.body = {points, total};
 });
 
+const PortfolioEdit = {
+    PortfolioEdit: [
+      { name: 'type', type: 'string' },
+      { name: 'category', type: 'uint256' },
+      { name: 'style', type: 'uint256' },
+      { name: 'id', type: 'uint256' },
+      { name: 'value', type: 'string[]' }
+  ]
+};
+
+const AccountEdit = {
+  Link: [
+    { name: 'name', type: 'string' },
+    { name: 'value', type: 'string' },
+  ],
+  AccountEdit: [
+    { name: 'type', type: 'string' },
+    { name: 'name', type: 'string' },
+    { name: 'avatar', type: 'string' },
+    { name: 'links', type: 'Link[]'}
+  ]
+};
+
+var types = {
+  PortfolioEdit,
+  AccountEdit
+};
+
 const router = new Router__default["default"]();
 
 const domain = {
--- packages/signer-types/types.js
+++ packages/signer-types/types.js
@@ -21,7 +21,7 @@ export const AccountEdit = {
   ]
 }
 
-export const type = {
+export default {
   PortfolioEdit,
   AccountEdit
 }
