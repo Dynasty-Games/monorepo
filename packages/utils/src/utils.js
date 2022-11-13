0bccd868aa454b34c305c6b8c42eb6589cd2b6ad
vandeurenglenn <vandeurenglenn@gmail.com>
2022-11-13 15:49:11 +0100

    utils: add fetchText

--- packages/utils/src/utils.js
+++ packages/utils/src/utils.js
@@ -2,15 +2,21 @@
 
 export const apiURL = 'https://api.dynastygames.games/'
 let _fetch
-const fetch = async (method, query) => {
+const fetch = async (method, query, options, type) => {
   if (!globalThis.fetch) {
     _fetch = await import('node-fetch')
     _fetch = _fetch.default
   }
   const url = `${apiURL}${method}${query ? `?${query}` : ''}`
-  const response = globalThis.fetch ? await globalThis.fetch(url) : await _fetch(url) 
+  const response = globalThis.fetch ? await globalThis.fetch(url, options) : await _fetch(url, options)
+  if (type) {
+    return response[type]()
+  }
   return response.json()
 }
+
+const fetchText = (method, query, options) => fetch(method, query, options, 'text')
+
 export const currencies = query => fetch('currencies', query)
 
 export const openCompetitions = query => fetch('open-competitions', query)
@@ -30,3 +36,11 @@ export const styles = query => fetch('styles', query)
 export const categories = query => fetch('categories', query)
 
 export const portfolioPoints = query => fetch('portfolio-points', query)
+
+export const avatar = query => fetchText('account/avatar', query)
+
+export const account = query => fetch('account', query)
+
+export const accountPortfolio = query => fetch('account/portfolio', query)
+
+export const accountPortfolioSave = query => fetch('account/portfolio', query, {method: 'put'})
