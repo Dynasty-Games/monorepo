1d304d215db73bc19035087bcecb4303b2ac18ee
vandeurenglenn <vandeurenglenn@gmail.com>
2022-11-13 15:48:57 +0100

    Add signer-types

---
+++ packages/signer-types/types.js
@@ -0,0 +1,27 @@
+export const PortfolioEdit = {
+    PortfolioEdit: [
+      { name: 'type', type: 'string' },
+      { name: 'category', type: 'uint256' },
+      { name: 'style', type: 'uint256' },
+      { name: 'id', type: 'uint256' },
+      { name: 'value', type: 'string[]' }
+  ]
+}
+
+export const AccountEdit = {
+  Link: [
+    { name: 'name', type: 'string' },
+    { name: 'value', type: 'string' },
+  ],
+  AccountEdit: [
+    { name: 'type', type: 'string' },
+    { name: 'name', type: 'string' },
+    { name: 'avatar', type: 'string' },
+    { name: 'links', type: 'Link[]'}
+  ]
+}
+
+export const type = {
+  PortfolioEdit,
+  AccountEdit
+}
